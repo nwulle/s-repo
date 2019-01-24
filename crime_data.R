45876bd88a23b6d6a308fedb71022dcd15f43542
Samantha Bansil <samanthabansil@Samanthas-Air.sciences-po.fr>
2019-01-24 12:02:43 +0100

    clean ups

--- crime_data.R
+++ crime_data.R
@@ -2,8 +2,6 @@
 
 library(dplyr)
 library(tidyverse)
-library(hrbrthemes)
-
 
 # Download data for burglaries in 2017 and 2018. Focus on departments: 75, 77, 78, 91, 92, 93, 94, 95
 
@@ -21,7 +19,6 @@ colnames(burglaries_france2018)[colnames(burglaries_france2018)=="221747"] <- "B
 colnames(burglaries_france2018)[colnames(burglaries_france2018)=="France"] <- "Insee"
 population2018 <- read_delim("~/Downloads/population2018.csv", 
                              ";", escape_double = FALSE, trim_ws = TRUE)
-View(population2018)
 
 #import Khartis basemap template
 library(readr)
@@ -31,7 +28,7 @@ france <- read_csv("data/Khartis_template_france-dept.csv")
 colnames(housing_prices)[colnames(housing_prices)=="Département"] <- "Insee"
 colnames(burglaries)[colnames(burglaries)=="Département"] <- "Insee"
 colnames(burglaries)[colnames(burglaries)=="Burglaries"] <- "Burglaries 2017"
-burglaries$Burglaries <- c("10089", "5944", "4352", "4807", "5272", "7640", "5662", "4500")        #values from final.csv (glitchy format)
+burglaries$Burglaries <- c("10089", "5944", "4352", "4807", "5272", "7640", "5662", "4500") #Burglaries in 2018 (from final.csv -- glitchy format)
 colnames(burglaries)[colnames(burglaries)=="Burglaries"] <- "Burglaries 2018"
 burglaries$`Burglaries 2018`<- as.numeric(burglaries$`Burglaries 2018`)
 
@@ -41,7 +38,7 @@ fr_house <- full_join(france, housing_prices)
 master <- full_join(fr_house, burglaries)
 fr_burg <- full_join(france, burglaries_france2018)
 master2 <- full_join(fr_burg, population2018)
-shortmaster <- head(master,7)
+shortmaster <- head(master,7) #khartis data for Ile de France
 
 #calculate burglary to population ratio
 shortmaster$burgpop2017 <- shortmaster$`Burglaries 2017`/shortmaster$Population
@@ -49,11 +46,11 @@ shortmaster$burgpop2018 <- shortmaster$`Burglaries 2018`/shortmaster$Population
 
 master2$Burglaries <- as.numeric(master2$Burglaries)
 master2$burgpop2018 <- master2$Burglaries/master2$`Population 2018`
-final_master<-head(master2,101)
+final_master<-head(master2,101) #Khartis data for all of France
 
 #export in CSV format --> upload to Khartis
-write.csv(final_master,"final_master2.csv")
-write.csv(shortmaster, "shortmaster2.csv")
+write.csv(final_master,"final_master2.csv") #France maps
+write.csv(shortmaster, "shortmaster2.csv") #IDF maps
 
 
 
