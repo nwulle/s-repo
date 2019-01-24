a181a19549c2f8b3a5c6409f7b4024eb5db88e32
Samantha Bansil <samanthabansil@Samanthas-Air.sciences-po.fr>
2019-01-24 09:33:47 +0100

    datathon day 1 code

--- crime_data.R
+++ crime_data.R
@@ -11,33 +11,78 @@ crime$a2018 <- rowSums(crime[,c(3:14)])
 crime$a2017 <- rowSums(crime[,c(15:26)])
 crime$a2000 <- rowSums(crime[,c(219:230)])
 
-crime %>% 
-  select(`libellé index`, a2018, a2017) %>% 
-  mutate(offence = fct_inorder(`libellé index`)) %>% 
-  mutate(coord = if_else(n > 22000, n - 1000, n + 1000),
-         colour = if_else(n > 22000, "white", "black")) %>% 
-  ggplot(aes(x = offence)) +
-  geom_bar(stat = "identity") +
-  scale_y_continuous(labels = scales::comma) +
-  geom_text(aes(label = offence)) +
-  scale_color_manual(values = c("black", "white"), guide = FALSE) +
-  xlab("") +
-  ylab("") +
-  ylim(0, NA) +
-  scale_x_discrete(labels = NULL)
-
-
-# Focus on departments: 75, 77, 78, 91, 92, 93, 94, 95
+# Download all crimes in 2017 and 2018. Focus on departments: 75, 77, 78, 91, 92, 93, 94, 95
 
 library(readxl)
-paris <- read_excel("data/tableaux-4001-ts.xlsx", 
+dep75 <- read_excel("data/tableaux-4001-ts.xlsx", 
                     sheet = "75")
-paris$a2018 <- rowSums(paris[,c(3:14)])
-paris$a2017 <- rowSums(paris[,c(15:26)])
+dep75$a2018 <- rowSums(dep75[,c(3:14)])
+dep75$a2017 <- rowSums(dep75[,c(15:26)])
 
-paris %>% 
-  select(`libellé index`, a2018, a2017) %>% 
-  View
+dep77 <- read_excel("data/tableaux-4001-ts.xlsx", 
+                    sheet = "77")
+dep77$a2018 <- rowSums(dep77[,c(3:14)])
+dep77$a2017 <- rowSums(dep77[,c(15:26)])
+
+dep78 <- read_excel("data/tableaux-4001-ts.xlsx", 
+                    sheet = "78")
+dep78$a2018 <- rowSums(dep78[,c(3:14)])
+dep78$a2017 <- rowSums(dep78[,c(15:26)])
+
+dep91 <- read_excel("data/tableaux-4001-ts.xlsx", 
+                    sheet = "91")
+dep91$a2018 <- rowSums(dep91[,c(3:14)])
+dep91$a2017 <- rowSums(dep91[,c(15:26)])
+
+dep92 <- read_excel("data/tableaux-4001-ts.xlsx", 
+                    sheet = "92")
+dep92$a2018 <- rowSums(dep92[,c(3:14)])
+dep92$a2017 <- rowSums(dep92[,c(15:26)])
+
+dep93 <- read_excel("data/tableaux-4001-ts.xlsx", 
+                    sheet = "93")
+dep93$a2018 <- rowSums(dep93[,c(3:14)])
+dep93$a2017 <- rowSums(dep93[,c(15:26)])
+
+dep94 <- read_excel("data/tableaux-4001-ts.xlsx", 
+                    sheet = "94")
+dep94$a2018 <- rowSums(dep94[,c(3:14)])
+dep94$a2017 <- rowSums(dep94[,c(15:26)])
+
+dep95 <- read_excel("data/tableaux-4001-ts.xlsx", 
+                    sheet = "95")
+dep95$a2018 <- rowSums(dep95[,c(3:14)])
+dep95$a2017 <- rowSums(dep95[,c(15:26)])
+
+
+#import burglaries and housing prices
+library(readxl)
+burglaries <- read_excel("data/Burglaries IDF.18.xls")
+housing_prices <- read_excel("data/housing prices.xlsx")
+
+
+#import Khartis basemap
+library(readr)
+france <- read_csv("data/Khartis_template_france-dept.csv")
+
+#merge datasets
+colnames(housing_prices)[colnames(housing_prices)=="Département"] <- "Insee"
+colnames(burglaries)[colnames(burglaries)=="Département"] <- "Insee"
+colnames(burglaries)[colnames(burglaries)=="Burglaries"] <- "Burglaries 2017"
+burglaries$Burglaries <- c("10089", "5944", "4352", "4807", "5272", "7640", "5662", "4500")
+colnames(burglaries)[colnames(burglaries)=="Burglaries"] <- "Burglaries 2018"
+
+housing_prices$Insee <- as.character(housing_prices$Insee)
+burglaries$Insee <- as.character(burglaries$Insee)
+fr_house <- full_join(france, housing_prices)
+master <- full_join(fr_house, burglaries)
+shortmaster <- head(master,7)
+
+
+
+
+
+
 
 
   
